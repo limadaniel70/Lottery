fac1ac00608bff0e2f4dfafc91c0e253d33ef53c
Daniel Lima <dnlima77237@gmail.com>
2024-08-23 22:51:23 -0300

    Updated db context logic

--- lottery/database.py
+++ lottery/database.py
@@ -1,7 +1,41 @@
 import sqlite3
-
+from multipledispatch import dispatch
 
 class DataBase:
 
-    def __init__(self) -> None:
+    def __init__(self, database_name: str) -> None:
+        self.connection = sqlite3.connect(database_name)
+        self.cursor = self.connection.cursor()
+
+    def add_player(self, player_name: str) -> None:
         pass
+
+    def add_players(self, players: list[str]) -> None:
+        pass
+
+    def get_player(self) -> str:
+        pass
+
+    def get_all_players(self) -> list[str]:
+        pass
+        
+    def remove_player(self, player_name: str) -> tuple[int, str]:
+        pass
+
+    @dispatch(list[str])
+    def remove_players(self, players: list[str]) -> dict[int, str]:
+        pass
+
+    @dispatch(*str)
+    def remove_players(self, *players: str) -> dict[int, str]:
+        pass
+
+    # def execute_custom_query(self, query: str) -> list[any]:
+    #    result = self.cursor.execute(query)
+    #    return result.fetchall()
+
+    def commit_changes(self) -> None:
+        self.connection.commit()
+
+    def close_connection(self) -> None:
+        self.connection.close
